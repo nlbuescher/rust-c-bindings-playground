d4224eeb2f4c623cb2755f4966c638f22a8f8dbd
Nico Büscher <nico@buescher.dev>
2025-03-05 13:27:18 +0100

    test trait objects
    since trait objects are not FFI-safe, they must be wrapped in an opaque pointer. This requires separate rust and C interface code.

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <rust.h>
+#include <stdio.h>
 
 int main(void) {
     rust_test();
-    return 0;
+
+    Shape* square = rust_shape_new_square(4);
+
+    const uint32_t area = rust_shape_area(square);
+    printf("area: %u\n", area);
+
+    rust_shape_free(square);
+
+    // second access causes segmentation fault, as it should
+    //const uint32_t area2 = rust_shape_area(square);
+    //printf("area: %u\n", area2);
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,4 +1,44 @@
-#[no_mangle]
-pub extern "C" fn rust_test() {
-    println!("Hello, Rust!");
+trait Shape {
+	fn area(&self) -> u32;
+}
+
+struct Square {
+	width: u32,
+}
+
+impl Square {
+	fn new(width: u32) -> Square {
+		Square { width }
+	}
+}
+
+impl Shape for Square {
+	fn area(&self) -> u32 { self.width * self.width }
+}
+
+mod sys {
+	#[no_mangle]
+	extern "C" fn rust_test() {
+		println!("Hello, Rust!");
+	}
+
+	struct Shape {
+		inner: Box<dyn crate::Shape>
+	}
+
+	#[no_mangle]
+	extern "C" fn rust_shape_new_square(width: u32) -> *mut Shape {
+		Box::into_raw(Box::new(Shape { inner: Box::new(super::Square { width }) }))
+	}
+
+	#[no_mangle]
+	unsafe extern "C" fn rust_shape_free(square: *mut Shape) {
+		let square = Box::from_raw(square);
+		drop(square);
+	}
+
+	#[no_mangle]
+	unsafe extern "C" fn rust_shape_area(shape: *mut Shape) -> u32 {
+		shape.as_ref().unwrap().inner.area()
+	}
 }
